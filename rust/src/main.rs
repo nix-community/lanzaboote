4970dafdbf7dcc1f51232903f50ec12562f220cf
Julian Stecklina <js@alien8.de>
2022-11-21 16:22:44 +0100

    Add logo

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -2,14 +2,32 @@
 #![no_std]
 #![feature(abi_efiapi)]
 
-use log::info;
-use uefi::prelude::*;
+use uefi::{prelude::*, proto::console::text::Output};
+
+mod boot_image;
+
+fn print_logo(output: &mut Output) {
+    output.clear().unwrap();
+
+    output
+        .output_string(cstr16!(
+            "
+  _                      _                 _   \r
+ | |                    | |               | |  \r
+ | | __ _ _ __  ______ _| |__   ___   ___ | |_ \r
+ | |/ _` | '_ \\|_  / _` | '_ \\ / _ \\ / _ \\| __|\r
+ | | (_| | | | |/ / (_| | |_) | (_) | (_) | |_ \r
+ |_|\\__,_|_| |_/___\\__,_|_.__/ \\___/ \\___/ \\__|\r
+"
+        ))
+        .unwrap();
+}
 
 #[entry]
 fn main(_handle: Handle, mut system_table: SystemTable<Boot>) -> Status {
     uefi_services::init(&mut system_table).unwrap();
 
-    info!("Hello World!");
+    print_logo(system_table.stdout());
 
     Status::SUCCESS
 }
