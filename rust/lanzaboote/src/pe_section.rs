b1ffddb0a6973f0082daa4364caa10e28c29cef5
Julian Stecklina <js@alien8.de>
2022-11-23 17:57:23 +0100

    Fix section extraction

--- rust/lanzaboote/src/pe_section.rs
+++ rust/lanzaboote/src/pe_section.rs
@@ -10,7 +10,9 @@ pub fn pe_section<'a>(file_data: &'a [u8], section_name: &str) -> Option<&'a [u8
         .find(|s| s.name().unwrap() == section_name)
         .and_then(|s| {
             let section_start: usize = s.pointer_to_raw_data.try_into().ok()?;
-            let section_end: usize = section_start + usize::try_from(s.size_of_raw_data).ok()?;
+
+            assert!(s.virtual_size <= s.size_of_raw_data);
+            let section_end: usize = section_start + usize::try_from(s.virtual_size).ok()?;
 
             Some(&file_data[section_start..section_end])
         })
