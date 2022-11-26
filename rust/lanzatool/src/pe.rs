fffa7d6bfa29de23e08f9a147d8355dba639cc0a
nikstur <nikstur@outlook.com>
2022-11-26 23:19:08 +0100

    lanzatool: appease clippy

--- rust/lanzatool/src/pe.rs
+++ rust/lanzatool/src/pe.rs
@@ -1,7 +1,7 @@
 use std::fs;
 use std::io::Write;
-use std::os::unix::prelude::OpenOptionsExt;
 use std::os::unix::fs::MetadataExt;
+use std::os::unix::prelude::OpenOptionsExt;
 use std::path::{Path, PathBuf};
 use std::process::Command;
 
@@ -23,18 +23,21 @@ pub fn lanzaboote_image(
 ) -> Result<PathBuf> {
     // objcopy copies files into the PE binary. That's why we have to write the contents
     // of some bootspec properties to disk
-    let (kernel_cmdline_file, _) = write_to_tmp(&target_dir,
-        "kernel-cmdline",
-        kernel_cmdline.join(" "))?;
-    let (kernel_path_file, _) = write_to_tmp(&target_dir, 
+    let (kernel_cmdline_file, _) =
+        write_to_tmp(target_dir, "kernel-cmdline", kernel_cmdline.join(" "))?;
+    let (kernel_path_file, _) = write_to_tmp(
+        target_dir,
         "kernel-esp-path",
-        esp_relative_path_string(esp, kernel_path))?;
-    let (initrd_path_file, _) = write_to_tmp(&target_dir, 
+        esp_relative_path_string(esp, kernel_path),
+    )?;
+    let (initrd_path_file, _) = write_to_tmp(
+        target_dir,
         "initrd-esp-path",
-        esp_relative_path_string(esp, initrd_path))?;
+        esp_relative_path_string(esp, initrd_path),
+    )?;
 
     let os_release_offs = stub_offset(lanzaboote_stub)?;
-    let kernel_cmdline_offs = os_release_offs + file_size(&os_release)?;
+    let kernel_cmdline_offs = os_release_offs + file_size(os_release)?;
     let initrd_path_offs = kernel_cmdline_offs + file_size(&kernel_cmdline_file)?;
     let kernel_path_offs = initrd_path_offs + file_size(&initrd_path_file)?;
 
@@ -45,7 +48,7 @@ pub fn lanzaboote_image(
         s(".kernelp", kernel_path_file, kernel_path_offs),
     ];
 
-    wrap_in_pe(&target_dir, "lanzaboote-stub.efi", &lanzaboote_stub, sections)
+    wrap_in_pe(target_dir, "lanzaboote-stub.efi", lanzaboote_stub, sections)
 }
 
 pub fn wrap_initrd(target_dir: &TempDir, initrd_stub: &Path, initrd: &Path) -> Result<PathBuf> {
@@ -54,7 +57,12 @@ pub fn wrap_initrd(target_dir: &TempDir, initrd_stub: &Path, initrd: &Path) -> R
     wrap_in_pe(target_dir, "wrapped-initrd.exe", initrd_stub, sections)
 }
 
-fn wrap_in_pe(target_dir: &TempDir, filename: &str, stub: &Path, sections: Vec<Section>) -> Result<PathBuf> {
+fn wrap_in_pe(
+    target_dir: &TempDir,
+    filename: &str,
+    stub: &Path,
+    sections: Vec<Section>,
+) -> Result<PathBuf> {
     let image_path = target_dir.path().join(filename);
     let _ = fs::OpenOptions::new()
         .create(true)
@@ -64,7 +72,10 @@ fn wrap_in_pe(target_dir: &TempDir, filename: &str, stub: &Path, sections: Vec<S
         .context("Failed to generate named temp file")?;
 
     let mut args: Vec<String> = sections.iter().flat_map(Section::to_objcopy).collect();
-    let extra_args = vec![utils::path_to_string(stub), utils::path_to_string(&image_path)];
+    let extra_args = vec![
+        utils::path_to_string(stub),
+        utils::path_to_string(&image_path),
+    ];
     args.extend(extra_args);
 
     let status = Command::new("objcopy")
@@ -72,7 +83,10 @@ fn wrap_in_pe(target_dir: &TempDir, filename: &str, stub: &Path, sections: Vec<S
         .status()
         .context("Failed to run objcopy command")?;
     if !status.success() {
-        return Err(anyhow::anyhow!("Failed to wrap in pe with args `{:?}`", &args).into());
+        return Err(anyhow::anyhow!(
+            "Failed to wrap in pe with args `{:?}`",
+            &args
+        ));
     }
 
     Ok(image_path)
@@ -103,7 +117,11 @@ fn s(name: &'static str, file_path: impl AsRef<Path>, offset: u64) -> Section {
     }
 }
 
-fn write_to_tmp(secure_temp: &TempDir, filename: &str, contents: impl AsRef<[u8]>) -> Result<(PathBuf, fs::File)> {
+fn write_to_tmp(
+    secure_temp: &TempDir,
+    filename: &str,
+    contents: impl AsRef<[u8]>,
+) -> Result<(PathBuf, fs::File)> {
     let mut tmpfile = fs::OpenOptions::new()
         .create(true)
         .write(true)
@@ -125,7 +143,7 @@ fn esp_relative_path_string(esp: &Path, path: &Path) -> String {
         .into_os_string()
         .into_string()
         .expect("Failed to convert path '{}' to a relative string path")
-        .replace("/", "\\");
+        .replace('/', "\\");
     format!("\\{}", &relative_path_string)
 }
 
@@ -140,7 +158,7 @@ fn stub_offset(binary: &Path) -> Result<u64> {
     Ok(u64::from(
         pe.sections
             .last()
-            .and_then(|s| Some(s.virtual_size + s.virtual_address))
+            .map(|s| s.virtual_size + s.virtual_address)
             .expect("Failed to calculate offset"),
     ) + image_base)
 }
