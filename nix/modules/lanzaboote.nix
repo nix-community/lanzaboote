37fa54aa37d63ca8c9dc473b7fee38d77c88250f
Raito Bezarius <masterancpp@gmail.com>
2022-12-26 02:02:11 +0100

    nixos/lanzaboote: support unsigned generation policies

--- nix/modules/lanzaboote.nix
+++ nix/modules/lanzaboote.nix
@@ -8,8 +8,22 @@ let
 in
 {
   options.boot.lanzaboote = {
-    enable = mkEnableOption "Enable the LANZABOOTE";
-    enrollKeys = mkEnableOption "Automatic enrollment of the keys using sbctl";
+    enable = mkEnableOption "LANZABOOTE";
+    enrollKeys = mkEnableOption "automatic enrollment of the keys using sbctl, DO NOT USE IF YOU DO NOT UNDERSTAND HOW IT WORKS, IT WILL BRICK YOUR MACHINE.";
+    unsignedGenerationsPolicy = mkOption {
+      type = types.enum [ "resign" "ignore" "resign-last-only" ];
+      default = "ignore";
+      description = ''
+        When introducing SecureBoot in your system, you will most likely have old generations
+        which are unsigned and will cease to boot.
+
+        Depending on your threat model, you may want to:
+
+        - resign all of them, ignoring all possibility of existing vulnerabilities in them
+        - resign only the last generation, reducing your risk to the very last one, useful if you want to have a known configuration with limited exposure
+        - ignore all of them, rendering your rollback feature unusable until SecureBoot is disabled or new generations are introduced
+      '';
+    };
     pkiBundle = mkOption {
       type = types.nullOr types.path;
       description = "PKI bundle containg db, PK, KEK";
@@ -49,6 +63,7 @@ in
         ${cfg.package}/bin/lanzatool install \
           --public-key ${cfg.publicKeyFile} \
           --private-key ${cfg.privateKeyFile} \
+          --unsigned-generations-policy ${cfg.unsignedGenerationsPolicy} \
           ${config.boot.loader.efi.efiSysMountPoint} \
           /nix/var/nix/profiles/system-*-link
       '';
