7af1c7ee97142542f5f9d8b681a7979574b238c0
Gauvain 'GovanifY' Roussel-Tarbouriech <gauvain@govanify.com>
2023-02-03 19:11:25 +0100

    grub-efi-image: generating default config

--- nix/packages/grub-efi-image.nix
+++ nix/packages/grub-efi-image.nix
@@ -5,7 +5,13 @@ stdenvNoCC.mkDerivation rec {
 
   buildCommand = ''
     mkdir $out
-    ${grub2_efi}/bin/grub-mkstandalone -O x86_64-efi -o $out/boot.efi --modules="part_gpt part_msdos"
+    cd /tmp
+    # this is very probably a terrible idea but grub doesn't allow to generate
+    # at runtime menus like systemd-boot does, so we will need to wait for
+    # lanzaboot to be ready to generate config for both sd-boot and grub.
+    echo -e 'chainloader (hd0,gpt1)/efi/Linux/nixos-generation-1.efi\nboot' > /tmp/grub.cfg
+    ${grub2_efi}/bin/grub-mkstandalone -O x86_64-efi -o $out/boot.efi \
+    --disable-shim-lock --modules="part_gpt part_msdos" "boot/grub/grub.cfg=/tmp/grub.cfg"
   '';
 
   meta = with lib; {
