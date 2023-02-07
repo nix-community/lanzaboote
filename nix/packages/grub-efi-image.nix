e68d4b935e880161220b430690bca21691fe16e3
Gauvain 'GovanifY' Roussel-Tarbouriech <gauvain@govanify.com>
2023-02-07 14:37:18 +0100

    grub: add unstable version with UKI patchset

--- nix/packages/grub-efi-image.nix
+++ nix/packages/grub-efi-image.nix
@@ -1,7 +1,33 @@
-{ lib, stdenvNoCC, grub2_efi }:
+{ lib, stdenvNoCC, grub2_efi, fetchFromGitHub, git, gnulib }:
+let
+  grub_uki = grub2_efi.overrideAttrs (finalAttrs: previousAttrs: rec {
+    patches = [ ];
+    src = fetchFromGitHub {
+      owner = "GovanifY";
+      repo = "grub";
+      rev = "master";
+      sha256 = "sha256-2XhZdRCK73QJ5QIeTdqXVFinzXq36pJ/6EwnluY5rnA=";
+    };
+
+
+    prePatch = ''
+      patchShebangs autogen.sh bootstrap
+      # copy gnulib into build dir and make writable.
+      # Otherwise ./bootstrap copies the non-writable files from nix store and fails to modify them
+      cp -r ${gnulib} gnulib
+      chmod -R u+w gnulib/{build-aux,lib}
+      ./bootstrap --no-git --gnulib-srcdir=gnulib --skip-po
+      ./autogen.sh
+    '';
+    nativeBuildInputs = previousAttrs.nativeBuildInputs ++ [ git ];
+    preConfigure = "";
+
+
+  });
+in
 stdenvNoCC.mkDerivation rec {
   pname = "grub-efi-image";
-  version = grub2_efi.version;
+  version = "unstable-uki";
 
   buildCommand = ''
     mkdir $out
@@ -9,9 +35,9 @@ stdenvNoCC.mkDerivation rec {
     # this is very probably a terrible idea but grub doesn't allow to generate
     # at runtime menus like systemd-boot does, so we will need to wait for
     # lanzaboot to be ready to generate config for both sd-boot and grub.
-    echo -e 'set root=(hd0,gpt2)\nchainloader (hd0,gpt1)/efi/Linux/nixos-generation-1.efi\nboot' > /tmp/grub.cfg
-    ${grub2_efi}/bin/grub-mkstandalone -O x86_64-efi -o $out/boot.efi \
-    --disable-shim-lock --modules="part_gpt part_msdos tpm" "boot/grub/grub.cfg=/tmp/grub.cfg"
+    echo -e 'set root=(hd0,gpt2)\nboot_loader_interface\nchainloader (hd0,gpt1)/efi/Linux/nixos-generation-1.efi\nboot' > /tmp/grub.cfg
+    ${grub_uki}/bin/grub-mkstandalone -O x86_64-efi -o $out/boot.efi \
+    --disable-shim-lock --modules="part_gpt part_msdos tpm boot_loader_interface" "boot/grub/grub.cfg=/tmp/grub.cfg"
   '';
 
   meta = with lib; {
