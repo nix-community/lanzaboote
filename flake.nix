0109e357aa8005fd1600c05d2711f393fa6da35e
nikstur <nikstur@outlook.com>
2023-07-22 00:45:50 +0200

    Revert "Merge pull request #147 from nix-community/nixpkgs-infrastructure"
    This reverts commit 59e3ebb19fdd3fd235d8275b008538a72872bad7, reversing
    changes made to 9f97a908e4059221d39c7b7d0906c88b9fcc9c9b.

--- flake.nix
+++ flake.nix
@@ -25,13 +25,27 @@
     # avoid having multiple versions in our dependencies.
     flake-utils.url = "github:numtide/flake-utils";
 
+    crane = {
+      url = "github:ipetkov/crane";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.rust-overlay.follows = "rust-overlay";
+      inputs.flake-utils.follows = "flake-utils";
+      inputs.flake-compat.follows = "flake-compat";
+    };
+
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.flake-utils.follows = "flake-utils";
+    };
+
     flake-compat = {
       url = "github:edolstra/flake-compat";
       flake = false;
     };
   };
 
-  outputs = inputs@{ self, nixpkgs, flake-parts, ... }:
+  outputs = inputs@{ self, nixpkgs, crane, rust-overlay, flake-parts, ... }:
     flake-parts.lib.mkFlake { inherit inputs; } ({ moduleWithSystem, ... }: {
       imports = [
         # Derive the output overlay automatically from all packages that we define.
@@ -41,20 +55,27 @@
         inputs.pre-commit-hooks-nix.flakeModule
       ];
 
-      flake.nixosModules.lanzaboote = moduleWithSystem (perSystem@{ config }:
+      flake.nixosModules.lanzaboote = moduleWithSystem (
+        perSystem@{ config }:
         { ... }: {
-          imports = [ ./nix/modules/lanzaboote.nix ];
+          imports = [
+            ./nix/modules/lanzaboote.nix
+          ];
 
           boot.lanzaboote.package = perSystem.config.packages.tool;
-        });
+        }
+      );
 
-      flake.nixosModules.uki = moduleWithSystem (perSystem@{ config }:
+      flake.nixosModules.uki = moduleWithSystem (
+        perSystem@{ config }:
         { lib, ... }: {
-          imports = [ ./nix/modules/uki.nix ];
+          imports = [
+            ./nix/modules/uki.nix
+          ];
 
-          boot.loader.uki.stub = lib.mkDefault
-            "${perSystem.config.packages.fatStub}/bin/lanzaboote_stub.efi";
-        });
+          boot.loader.uki.stub = lib.mkDefault "${perSystem.config.packages.fatStub}/bin/lanzaboote_stub.efi";
+        }
+      );
 
       systems = [
         "x86_64-linux"
@@ -65,32 +86,94 @@
 
       perSystem = { config, system, pkgs, ... }:
         let
-          pkgs = import nixpkgs { inherit system; };
-          uefiPkgs = import nixpkgs {
-            inherit system;
-            crossSystem = {
-              # linuxArch is wrong here, it will yield arm64 instead of aarch64.
-              config = "${pkgs.stdenv.hostPlatform.qemuArch}-windows";
-              rustc.config = "${pkgs.stdenv.hostPlatform.qemuArch}-unknown-uefi";
-              libc = null;
-              useLLVM = true;
-            };
+          pkgs = import nixpkgs {
+            system = system;
+            overlays = [
+              rust-overlay.overlays.default
+            ];
           };
-          utils = import ./nix/packages/utils.nix;
 
           inherit (pkgs) lib;
 
-          stub = uefiPkgs.callPackage ./nix/packages/stub.nix {
-            # cargo-auditable fails to build with: could not execute process .... No such file or directory (os error 2)
-            rustPlatform = uefiPkgs.makeRustPlatform {
-              inherit (uefiPkgs.buildPackages) rustc;
-              cargo = uefiPkgs.buildPackages.cargo.override {
-                auditable = false;
-              };
+          uefi-rust-stable = pkgs.rust-bin.fromRustupToolchainFile ./rust/stub/rust-toolchain.toml;
+          craneLib = crane.lib.x86_64-linux.overrideToolchain uefi-rust-stable;
+
+          # Build attributes for a Rust application.
+          buildRustApp = lib.makeOverridable (
+            { src
+            , target ? null
+            , doCheck ? true
+            , extraArgs ? { }
+            }:
+            let
+              commonArgs = {
+                inherit src;
+                CARGO_BUILD_TARGET = target;
+                inherit doCheck;
+
+                # Workaround for https://github.com/ipetkov/crane/issues/262.
+                dummyrs = pkgs.writeText "dummy.rs" ''
+                  #![allow(unused)]
+
+                  #![cfg_attr(
+                    any(target_os = "none", target_os = "uefi"),
+                    no_std,
+                    no_main,
+                  )]
+
+                  #[cfg_attr(any(target_os = "none", target_os = "uefi"), panic_handler)]
+                  fn panic(_info: &::core::panic::PanicInfo<'_>) -> ! {
+                      loop {}
+                  }
+
+                  #[cfg_attr(any(target_os = "none", target_os = "uefi"), export_name = "efi_main")]
+                  fn main() {}
+                '';
+              } // extraArgs;
+
+              cargoArtifacts = craneLib.buildDepsOnly commonArgs;
+            in
+            {
+              package = craneLib.buildPackage (commonArgs // {
+                inherit cargoArtifacts;
+              });
+
+              clippy = craneLib.cargoClippy (commonArgs // {
+                inherit cargoArtifacts;
+                cargoClippyExtraArgs = "-- --deny warnings";
+              });
+
+              rustfmt = craneLib.cargoFmt (commonArgs // { inherit cargoArtifacts; });
+            }
+          );
+
+          stubCrane = buildRustApp {
+            src = craneLib.cleanCargoSource ./rust/stub;
+            target = "x86_64-unknown-uefi";
+            doCheck = false;
+          };
+
+          fatStubCrane = stubCrane.override {
+            extraArgs = {
+              cargoExtraArgs = "--no-default-features --features fat";
             };
           };
-          fatStub = stub.override { fatVariant = true; };
-          tool = pkgs.callPackage ./nix/packages/tool.nix { };
+
+          stub = stubCrane.package;
+          fatStub = fatStubCrane.package;
+
+          toolCrane = buildRustApp {
+            src = ./rust/tool;
+            extraArgs = {
+              TEST_SYSTEMD = pkgs.systemd;
+              nativeCheckInputs = with pkgs; [
+                binutils-unwrapped
+                sbsigntool
+              ];
+            };
+          };
+
+          tool = toolCrane.package;
 
           wrappedTool = pkgs.runCommand "lzbt"
             {
@@ -101,9 +184,7 @@
             # Clean PATH to only contain what we need to do objcopy. Also
             # tell lanzatool where to find our UEFI binaries.
             makeWrapper ${tool}/bin/lzbt $out/bin/lzbt \
-              --set PATH ${
-                lib.makeBinPath [ pkgs.binutils-unwrapped pkgs.sbsigntool ]
-              } \
+              --set PATH ${lib.makeBinPath [ pkgs.binutils-unwrapped pkgs.sbsigntool ]} \
               --set LANZABOOTE_STUB ${stub}/bin/lanzaboote_stub.efi
           '';
         in
@@ -114,23 +195,24 @@
             lzbt = wrappedTool;
           };
 
-          overlayAttrs = { inherit (config.packages) tool; };
+          overlayAttrs = {
+            inherit (config.packages) tool;
+          };
 
           checks =
             let
               nixosLib = import (pkgs.path + "/nixos/lib") { };
-              runTest = module:
-                nixosLib.runTest {
-                  imports = [ module ];
-                  hostPkgs = pkgs;
-                };
+              runTest = module: nixosLib.runTest {
+                imports = [ module ];
+                hostPkgs = pkgs;
+              };
             in
             {
-              stubFmt = uefiPkgs.callPackage (utils.rustfmt stub) { };
-              toolFmt = pkgs.callPackage (utils.rustfmt tool) { };
-              toolClippy = pkgs.callPackage (utils.clippy tool) { };
-              stubClippy = uefiPkgs.callPackage (utils.clippy stub) { };
-              fatStubClippy = uefiPkgs.callPackage (utils.clippy fatStub) { };
+              toolClippy = toolCrane.clippy;
+              stubClippy = stubCrane.clippy;
+              fatStubClippy = fatStubCrane.clippy;
+              toolFmt = toolCrane.rustfmt;
+              stubFmt = stubCrane.rustfmt;
             } // (import ./nix/tests/lanzaboote.nix {
               inherit pkgs;
               lanzabooteModule = self.nixosModules.lanzaboote;
@@ -174,7 +256,10 @@
               pkgs.cargo-release
             ];
 
-            inputsFrom = [ config.packages.stub config.packages.tool ];
+            inputsFrom = [
+              config.packages.stub
+              config.packages.tool
+            ];
 
             TEST_SYSTEMD = pkgs.systemd;
           };
