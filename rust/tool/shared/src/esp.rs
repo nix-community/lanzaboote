ff442cd03266f8b982a7eb7f40c38c88c62d1f7d
nikstur <nikstur@outlook.com>
2023-09-14 12:38:40 +0200

    tool: introduce some more whitespace

--- rust/tool/shared/src/esp.rs
+++ rust/tool/shared/src/esp.rs
@@ -11,10 +11,13 @@ use crate::generation::Generation;
 pub trait EspPaths<const N: usize> {
     /// Build an ESP path structure out of the ESP root directory
     fn new(esp: impl AsRef<Path>) -> Self;
+
     /// Return the used file paths to store as garbage collection roots.
     fn iter(&self) -> std::array::IntoIter<&PathBuf, N>;
+
     /// Returns the path containing NixOS EFI binaries
     fn nixos_path(&self) -> &Path;
+
     /// Returns the path containing Linux EFI binaries
     fn linux_path(&self) -> &Path;
 }
