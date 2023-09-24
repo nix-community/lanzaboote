fd7b0e50d1b41603187bd980b1ce457e33041415
Raito Bezarius <masterancpp@gmail.com>
2023-09-24 19:55:59 +0200

    tool(systemd): introduce `lzbt build` to build stubs on the command line
    This is useful when you want to build a netboot stub which will
    continue the control after being PXE booted.

--- rust/tool/systemd/src/cli.rs
+++ rust/tool/systemd/src/cli.rs
@@ -1,11 +1,13 @@
-use std::path::PathBuf;
+use std::{path::PathBuf, io::Write};
 
 use anyhow::{Context, Result};
 use clap::{Parser, Subcommand};
+use tempfile::TempDir;
 
 use crate::install;
 use lanzaboote_tool::architecture::Architecture;
 use lanzaboote_tool::signature::KeyPair;
+use lanzaboote_tool::generation::{GenerationLink, Generation}, pe, os_release::OsRelease, utils::{SecureTempDirExt, assemble_kernel_cmdline}, esp::{EspGenerationPaths, BuildEspPaths, EspPaths}};
 
 /// The default log level.
 ///
@@ -27,6 +29,25 @@ pub struct Cli {
 #[derive(Subcommand)]
 enum Commands {
     Install(InstallCommand),
+    Build(BuildCommand)
+}
+
+#[derive(Parser)]
+struct BuildCommand {
+    /// sbsign Public Key
+    #[arg(long)]
+    public_key: PathBuf,
+
+    /// sbsign Private Key
+    #[arg(long)]
+    private_key: PathBuf,
+
+    /// Override initrd
+    #[arg(long)]
+    initrd: PathBuf,
+
+    /// Generation
+    generation: PathBuf
 }
 
 #[derive(Parser)]
@@ -83,6 +104,7 @@ impl Commands {
     pub fn call(self) -> Result<()> {
         match self {
             Commands::Install(args) => install(args),
+            Commands::Build(args) => build(args),
         }
     }
 }
@@ -104,4 +126,49 @@ fn install(args: InstallCommand) -> Result<()> {
         args.generations,
     )
     .install()
+}
+
+fn build(args: BuildCommand) -> Result<()> {
+    let lanzaboote_stub =
+        PathBuf::from(std::env::var("LANZABOOTE_STUB").context("Failed to read LANZABOOTE_STUB env variable")?);
+
+    let key_pair = KeyPair::new(&args.public_key, &args.private_key);
+
+    let generation = Generation::from_toplevel(&args.generation, 1)
+        .with_context(|| format!("Failed to build generation from link: {0:?}", args.generation))?;
+    let bootspec = &generation.spec.bootspec.bootspec;
+
+    let tempdir = TempDir::new().context("Failed to create temporary directory")?;
+    let os_release = OsRelease::from_generation(&generation)
+        .context("Failed to build OsRelease from generation.")?;
+    let os_release_path = tempdir
+        .write_secure_file(os_release.to_string().as_bytes())
+        .context("Failed to write os-release file.")?;
+    let kernel_cmdline =
+        assemble_kernel_cmdline(&bootspec.init, bootspec.kernel_params.clone());
+    let esp = PathBuf::from("/");
+    let esp_paths = BuildEspPaths::new("/");
+    let esp_gen_paths = EspGenerationPaths::new(&esp_paths, &generation)?;
+
+    let lzbt_stub = pe::lanzaboote_image(
+        &tempdir,
+        &lanzaboote_stub,
+        &os_release_path,
+        &kernel_cmdline,
+        &bootspec.kernel,
+        &args.initrd,
+        &esp_gen_paths,
+        &esp
+    )?;
+
+    // Sign the stub.
+    let to = tempdir.path().join("signed-lzbt-stub.efi");
+    key_pair
+        .sign_and_copy(&lzbt_stub, &to)
+        .with_context(|| format!("Failed to copy and sign file {lzbt_stub:?} to {to:?}"))?;
+
+    // Output the stub on stdout.
+    std::io::stdout().write_all(&std::fs::read(to)?)?;
+
+    Ok(())
 }
