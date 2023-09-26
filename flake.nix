3dfa22b1dc0823a037c2627510326da4d0cdcc17
Raito Bezarius <masterancpp@gmail.com>
2023-09-27 01:20:25 +0200

    nix/tests: extract into a lanzaboote library some common functions
    Our lanzaboote integration tests are getting more and more sophisticated and ambitious.
    
    Let's extract them into a "lanzalib", so they can be used with multiple backends.

--- flake.nix
+++ flake.nix
@@ -217,6 +217,10 @@
           checks =
             let
               nixosLib = import (pkgs.path + "/nixos/lib") { };
+              lanzaLib = import ./nix/tests/lib.nix {
+                inherit pkgs;
+                lanzabooteModule = self.nixosModules.lanzaboote;
+              };
               runTest = module: nixosLib.runTest {
                 imports = [ module ];
                 hostPkgs = pkgs;
@@ -229,7 +233,7 @@
               toolFmt = toolCrane.rustfmt;
               stubFmt = stubCrane.rustfmt;
             } // (import ./nix/tests/lanzaboote.nix {
-              inherit pkgs;
+              inherit pkgs lanzaLib;
               lanzabooteModule = self.nixosModules.lanzaboote;
             }) // (import ./nix/tests/stub.nix {
               inherit pkgs runTest;
