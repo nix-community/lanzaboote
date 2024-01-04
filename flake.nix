5dad0686527b0cc48e43a8e8880dbe83ff44ae03
Raito Bezarius <masterancpp@gmail.com>
2024-01-04 23:26:55 +0100

    [DO NOT MERGE] fix: do not emit a temporary initrd location if it's not needed
    We should wait on the upstream PR to be merged so we can get rid of the
    flake reference.

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Secure Boot for NixOS";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable-small";
+    nixpkgs.url = "github:RaitoBezarius/nixpkgs/initrd-secrets";
 
     flake-parts.url = "github:hercules-ci/flake-parts";
     flake-parts.inputs.nixpkgs-lib.follows = "nixpkgs";
