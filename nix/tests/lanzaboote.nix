14afe7ce9b71560e2edd26ac78bba32145c0b593
Julian Stecklina <js@alien8.de>
2024-02-10 20:52:22 +0100

    tests: check whether our UKIs are recognized

--- nix/tests/lanzaboote.nix
+++ nix/tests/lanzaboote.nix
@@ -202,6 +202,10 @@ in
     testScript = ''
       machine.start()
       assert "Secure Boot: enabled (user)" in machine.succeed("bootctl status")
+
+      # We want systemd to recognize our PE binaries as true UKIs. systemd has
+      # become more picky in the past, so make sure.
+      assert "Kernel Type: uki" in machine.succeed("bootctl kernel-inspect /boot/EFI/Linux/nixos-generation-1-*.efi")
     '';
   };
 
