2cee36f14d9016b3d9147ffd0bc76dfb965b10e1
Raito Bezarius <masterancpp@gmail.com>
2024-06-10 20:14:46 +0200

    stub: simplify `compute_pad4`
    Hopefully, compiler optimizations will avoid the vector allocation path
    for already-aligned buffers.
    
    Signed-off-by: Raito Bezarius <masterancpp@gmail.com>

--- rust/uefi/stub/src/thin.rs
+++ rust/uefi/stub/src/thin.rs
@@ -143,28 +143,18 @@ pub fn boot_linux(
 
     /// Compute the necessary padding based on the provided length
     /// It returns None if no padding is necessary.
-    fn compute_pad4(len: usize) -> Option<Vec<u8>> {
-        let overhang = len % 4;
-        if overhang != 0 {
-            let repeat = 4 - overhang;
-            Some(vec![0u8; repeat])
-        } else {
-            None
-        }
-    }
-    if let Some(mut padding) = compute_pad4(initrd_data.len()) {
-        initrd_data.append(&mut padding);
+    fn compute_pad4(len: usize) -> Vec<u8> {
+        vec![0u8; (4 - (len % 4)) % 4]
     }
 
+    initrd_data.append(&mut compute_pad4(initrd_data.len()));
     for mut extra_initrd in dynamic_initrds {
         // Uncomment for maximal debugging pleasure.
         // let debug_representation = extra_initrd.as_slice().escape_ascii().collect::<Vec<u8>>();
         // log::warn!("{:?}", String::from_utf8_lossy(&debug_representation));
         initrd_data.append(&mut extra_initrd);
         // Extra initrds ideally should be aligned, but just in case, let's verify this.
-        if let Some(mut padding) = compute_pad4(initrd_data.len()) {
-            initrd_data.append(&mut padding);
-        }
+        initrd_data.append(&mut compute_pad4(initrd_data.len()));
     }
 
     boot_linux_unchecked(handle, system_table, kernel_data, &cmdline, initrd_data)
