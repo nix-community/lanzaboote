bf7bbe8e6d22fefd037cc6b1f92e0bbe85db8ea3
2goodAP <45424904+2goodAP@users.noreply.github.com>
2025-08-10 23:33:38 +0545

    Update existing rust test cases to better cover profile integration

--- rust/tool/systemd/tests/integration/os_release.rs
+++ rust/tool/systemd/tests/integration/os_release.rs
@@ -1,7 +1,7 @@
 use std::fs;
 
 use anyhow::{Context, Result};
-use expect_test::expect;
+use expect_test::{expect, Expect};
 use tempfile::tempdir;
 
 use crate::common;
@@ -13,25 +13,47 @@ fn generate_expected_os_release() -> Result<()> {
     let profiles = tempdir()?;
     let toplevel = common::setup_toplevel(tmpdir.path())?;
 
-    let generation_link =
-        common::setup_generation_link_from_toplevel(&toplevel, profiles.path(), 1)
-            .expect("Failed to setup generation link");
+    let gen_exp_os_rel = |prof: Option<String>, ver: u64, exp: Expect| -> Result<()> {
+        let generation_link = common::setup_generation_link_from_toplevel(
+            &toplevel,
+            profiles.path(),
+            ver,
+            prof.clone(),
+        )
+        .expect("Failed to setup generation link");
 
-    let output0 = common::lanzaboote_install(0, esp_mountpoint.path(), vec![generation_link])?;
-    assert!(output0.status.success());
+        let output0 = common::lanzaboote_install(0, esp_mountpoint.path(), vec![generation_link])?;
+        assert!(output0.status.success());
 
-    let stub_data = fs::read(common::image_path(&esp_mountpoint, 1, &toplevel)?)?;
-    let os_release_section = pe_section(&stub_data, ".osrel")
-        .context("Failed to read .osrelease PE section.")?
-        .to_owned();
+        let stub_data = fs::read(common::image_path(&esp_mountpoint, ver, prof, &toplevel)?)?;
+        let os_release_section = pe_section(&stub_data, ".osrel")
+            .context("Failed to read .osrelease PE section.")?
+            .to_owned();
 
+        exp.assert_eq(&String::from_utf8(os_release_section)?);
+
+        Ok(())
+    };
+
+    // Without profile
     let expected = expect![[r#"
         ID=lanzaboote
         PRETTY_NAME=LanzaOS (Generation 1, 1970-01-01)
         VERSION_ID=Generation 1, 1970-01-01
     "#]];
+    let _ = gen_exp_os_rel(None, 1u64, expected);
 
-    expected.assert_eq(&String::from_utf8(os_release_section)?);
+    // With profile
+    let expected_prof = expect![[r#"
+        ID=lanzaboote
+        PRETTY_NAME=LanzaOS [My W#@cky_Yet_L3g!t profile-name -3] (Generation 1, 1970-01-01)
+        VERSION_ID=Generation 1, 1970-01-01
+    "#]];
+    let _ = gen_exp_os_rel(
+        Some("My W#@cky_Yet_L3g!t profile-name -3".to_string()),
+        1u64,
+        expected_prof,
+    );
 
     Ok(())
 }
